eb1a1dd1c3ba34c1b25dbe2bd6b85d0041f3b6e0
Jack <jackowendickson@gmail.com>
2022-03-04 12:54:39 +0000

    Carried out tests where guest objects where taking on the desired values

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from tests.song_test import SongTest
+from tests.guest_test import GuestTest
 
 if __name__ == '__main__':
     unittest.main()
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,2 +1,9 @@
 import unittest
-# from src.guest import Guest
+from src.guest import Guest
+
+class GuestTest(unittest.TestCase):
+    def setUp(self):
+        self.guest1 = Guest("Kiryu Kazuma")
+    
+    def test_guest_has_name(self):
+        self.assertEqual("Kiryu Kazuma", self.guest1.name)
