5ca88140758645f0bb026367695d7897ba664517
Jack <jackowendickson@gmail.com>
2022-03-04 15:42:56 +0000

    Created and Tested a couple of methods for Room

--- src/room.py
+++ src/room.py
@@ -3,3 +3,14 @@ class Room:
         self.room_number = room_number
         self.guests = []
         self.current_song = None
+        self.song_queue = []
+    
+    def add_guest(self, guest):
+        self.guests.append(guest)
+
+    def check_current_song(self):
+        return self.current_song
+
+    # def queue_song(self, song):
+    
+
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,9 +1,13 @@
 import unittest
 from src.room import Room
+from src.guest import Guest
+from src.song import Song
 
 class RoomTest(unittest.TestCase):
     def setUp(self):
         self.room1 = Room(1)
+        self.guest1 = Guest("Kiryu Kazuma")
+        self.song1 = Song("Judgement", 227, "rock")
     
     def test_room_has_number(self):
         self.assertEqual(1, self.room1.room_number)
@@ -13,3 +17,21 @@ class RoomTest(unittest.TestCase):
     
     def test_room_has_no_songs_playing(self):
         self.assertEqual(None, self.room1.current_song)
+    
+    def test_room_has_empty_song_queue(self):
+        self.assertEqual(0, len(self.room1.song_queue))
+
+#TESTING METHODS
+
+    def test_room_has_guest(self):
+        self.room1.add_guest(self.guest1)
+
+        self.assertEqual(1, len(self.room1.guests))
+    
+    def test_check_current_song_when_nothing_has_been_queued(self):
+        check_song = self.room1.check_current_song()
+
+        self.assertEqual(check_song, self.room1.current_song)
+    
+    # def test_check_current_song_when_song_is_queued(self):
+        
