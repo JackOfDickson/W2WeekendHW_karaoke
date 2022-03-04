594390ba8075df79ec9d78dfa3d29707a8d154b6
Jack <jackowendickson@gmail.com>
2022-03-04 12:48:02 +0000

    Set up the and completed the tests to show the song object was taking on the desired inputs

--- tests/song_test.py
+++ tests/song_test.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 import unittest
 from src.song import Song
 
@@ -6,9 +5,11 @@ class SongTest(unittest.TestCase):
     def setUp(self):
         self.song1 = Song("Judgement", 227, "rock")
     
-    def song_has_name(self):
-        self.assertEqual("Judgement", self.song1(name))
+    def test_song_has_name(self):
+        self.assertEqual("Judgement", self.song1.title)
 
-    # def song_has_duration(self):
+    def test_song_has_duration(self):
+        self.assertEqual(227, self.song1.duration)
 
-    # def song_has_genre(self):
+    def test_song_has_genre(self):
+        self.assertEqual("rock", self.song1.genre)
